67824c26322741ca8108ace26fb0f1e103e8784c
Max Gregory <max.gregory@chrobinson.com>
2021-04-20 18:04:46 +0000

    orders model added

---
+++ models/orders.sql
@@ -0,0 +1,40 @@
+with Orders as (
+
+    select * from {{ ref('stg_orders') }}
+
+),
+
+Payments as (
+
+    select * from {{ ref('stg_payments') }}
+
+),
+
+Order_Payments AS(
+
+    SELECT 
+        SUM(Amount) AS Amount,
+        order_id
+    FROM Payments
+    WHERE
+        Status <> 'FAIL'
+    Group BY
+        order_id
+
+), 
+
+Final AS(
+
+    SELECT 
+        O.customer_id,
+        O.order_id,
+        O.status,
+        OP.Amount
+    FROM 
+        Orders O
+        LEFT JOIN Order_Payments OP 
+            ON O.order_id = OP.order_id
+
+)
+
+SELECT * FROM Final
---
+++ models/stg_payments.sql
@@ -0,0 +1,10 @@
+SELECT 
+    ID AS Payment_ID,
+    OrderID AS order_id,
+    PaymentMethod,
+    (Amount/100) AS Amount,
+    Status,
+    Created AS Created_At,
+    _Batched_At
+FROM 
+    raw.stripe.payment 
